be162adc30248ca95c6dc530485b18916ca3d9d2
Валерон <valera-sergeev2@yandex.ru>
2021-02-17 17:18:43 +0500

    master

--- semester2/laba1/01.py
+++ semester2/laba1/01.py
@@ -1,9 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
+plt.style.use("seaborn")
 
-x = np.linespace(-3 , 3, 50)
-y = (x**2 + y**2) / 3*x
+x = np.linspace(-5 , 5, 100)
+y = 6.5*x**6 - 250
 
 fig, ax = plt.subplots()
-ax.plot(x, y)
+
+ax.plot(x, y, color = "aqua", lw = "4")
+ax.vlines(0, y.min(), y.max(), color = "palegoldenrod", lw = "2")
+ax.hlines(0, x.min(), x.max(), color = "palegoldenrod", lw = "2")
+
+fig.set_figwidth(4); fig.set_figheight(8)
 
 plt.show()
