16da653786701a69ff1a02f70d7098e4f4acf3a5
Валерон <valera-sergeev2@yandex.ru>
2021-02-17 19:01:31 +0500

    master

--- semester2/laba1/01(vid1).py
+++ semester2/laba1/01(vid1).py
@@ -4,13 +4,13 @@ import numpy as np
 plt.style.use("seaborn")
 
 x = np.linspace(-5 , 5, 100)
-y = 6.5*x**6 - 250
+y = 6.8*x**7 - 
 
 fig, ax = plt.subplots()
 
-ax.plot(x, y, color = "aqua", lw = "4")
-ax.vlines(0, y.min(), y.max(), color = "palegoldenrod", lw = "2")
-ax.hlines(0, x.min(), x.max(), color = "palegoldenrod", lw = "2")
+ax.plot(x, y, color = "aqua", lw = 4 )
+ax.vlines(0, y.min(), y.max(), color = "red", lw = 2 )
+ax.hlines(0, x.min(), x.max(), color = "red", lw = 2 )
 
 fig.set_figwidth(4); fig.set_figheight(8)
 
