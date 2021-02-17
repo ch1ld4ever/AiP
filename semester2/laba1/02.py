5555cecdabfaf695f94afd479157cc802769c8c6
Валерон <valera-sergeev2@yandex.ru>
2021-02-17 17:43:48 +0500

    master

--- semester2/laba1/02.py
+++ semester2/laba1/02.py
@@ -9,7 +9,7 @@ def my_graph(x, a, b, c):
 
 
 a = -5; b = 200 ; c = 100
-legend = "y = a*(2*x**4) + b*(x**2) + c"
+legend = "y = {}*(2*x**4) + {}*(x**2) + {}".format(a, b, c)
 left = -100; right = 100; step = .1
 
 data_x = np.arange(left, right, step)
@@ -21,4 +21,6 @@ plt.xlabel("Ось X")
 plt.ylabel("Ось Y")
 plt.legend([legend])
 plt.grid(True)
+plt.axhline()
+plt.axvline()
 plt.show()
